418a1969582c2b71ee37efe97605150723f9ed04
Maame Abena Afriyie <159795689+maameabena101@users.noreply.github.com>
2026-03-13 22:55:52 +0000

    Add imports for login and case history screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/landing_page.dart';
+import 'screens/login_page.dart';
 import 'screens/sign_up_page.dart';
+import 'screens/case_history.dart';
 
 void main() {
 }
