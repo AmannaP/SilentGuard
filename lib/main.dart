0ab398f1f7f97d5f4d9db441cff23eb24e011047
Rafiatou Malam Ali <rafiatoumalamali@gmail.com>
2026-03-27 11:35:49 +0000

    new updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:firebase_core/firebase_core.dart';
-=======
 import 'screens/profile.dart';
->>>>>>> 7d99e79e84dea3781a33b5662d6f7cf5a88beeef
 import 'screens/landing_page.dart';
 import 'screens/login_page.dart';
 import 'screens/sign_up_page.dart';
 import 'screens/case_history.dart';
-<<<<<<< HEAD
-import 'screens/home_screen.dart';
-import 'screens/map_tracking_screen.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp();
-  } catch (e) {
-    debugPrint("Firebase initialization failed: $e");
-  }
-=======
 import 'screens/case_screens.dart';
 import 'screens/home_screen.dart';
 import 'screens/map_tracking_screen.dart';
@@ -28,14 +13,18 @@ import 'screens/upload_evidence_screen.dart';
 import 'screens/record_evidence_screen.dart';
 import 'screens/call_screen.dart';
 
-void main() {
->>>>>>> 7d99e79e84dea3781a33b5662d6f7cf5a88beeef
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    debugPrint("Firebase initialization failed: $e");
+  }
   runApp(const SilentGuardApp());
 }
 
 class SilentGuardApp extends StatelessWidget {
   const SilentGuardApp({super.key});
-<<<<<<< HEAD
 
   @override
   Widget build(BuildContext context) {
@@ -46,43 +35,22 @@ class SilentGuardApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFCD7F32)),
       ),
-      // Changed initialRoute from '/' to '/home_page'
-      initialRoute: '/home_page',
-=======
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SilentGuard',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, primarySwatch: Colors.orange),
       // Set the starting page
-      initialRoute: '/',
->>>>>>> 7d99e79e84dea3781a33b5662d6f7cf5a88beeef
+      initialRoute: '/home_page',
       routes: {
         '/': (context) => const LandingPage(),
         '/login_page': (context) => const LoginPage(),
         '/sign_up_page': (context) => const SignUpPage(),
-<<<<<<< HEAD
-        '/home_page': (context) => const HomeScreen(),
-        '/case_history': (context) => const CaseHistory(),
-        '/map_tracking': (context) => const MapTrackingScreen(),
-      },
-    );
-  }
-}
-=======
         '/case_screens': (context) => const CaseScreens(),
         '/profile': (context) => const ProfilePage(),
         '/case_history': (context) => const CaseHistory(),
-        '/home_screen': (context) => const HomeScreen(),
-        '/map_tracking_screen': (context) => const MapTrackingScreen(),
+        '/home_page': (context) => const HomeScreen(),
+        '/map_tracking': (context) => const MapTrackingScreen(),
         '/archive_screen': (context) => const ArchiveScreen(),
         '/call_screen': (context) => const CallScreen(),
         '/upload_evidence_screen': (context) => const UploadEvidenceScreen(),
         '/record_evidence_screen': (context) => const RecordEvidenceScreen(),
       },
-
     );
   }
 }
->>>>>>> 7d99e79e84dea3781a33b5662d6f7cf5a88beeef
